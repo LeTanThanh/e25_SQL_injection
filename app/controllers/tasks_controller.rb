ce59b0aedb98c63f4f2d7b0b72163497e8ceb358
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-25 14:03:37 +0700

    e25_SQL_injection

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
   def index
-    if params[:query]
-      @tasks = Task.all
+    if params[:query].present?
+      # error SQL syntax with query = name' name
+      # @tasks = Task.where("name LIKE '%#{params[:query]}%'")
+
+      @tasks = Task.where("name LIKE :query", query: "%#{params[:query]}%")
     else
       @tasks = Task.all
     end
